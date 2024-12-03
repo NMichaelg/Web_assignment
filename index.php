2c16086c6cbfdeea66795ebcdf7abc8957a7a478
Anhinla <huynhnguyenngocanh91thcsduclap@gmail.com>
2024-12-03 11:21:36 +0700

    fix

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ session_start()
         <nav>
           <ul class="nav" id="navBarItem">
             <li class="nav-item"><a href="index.php?page=home" class="nav-link text-white">Home</a></li>
-            <li class="nav-item"><a href="index.php?page=your_cv" class="nav-link text-white">Make new CV</a></li>
+            <li class="nav-item"><a href="index.php?page=your_cv" class="nav-link text-white">Make / Edit CV</a></li>
             <li class="nav-item"><a href="index.php?page=contact_us" class="nav-link text-white">Contact Us</a></li>
             <?php
               if(isset($_SESSION['username'])){
